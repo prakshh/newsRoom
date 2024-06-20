16a2241363876c99ddd15699956b0c3d43bb77cb
prakshh <prakashdas18@gmail.com>
2024-06-20 15:34:44 +0530

    display data on LWC

--- force-app/main/default/lwc/newsComponent/newsComponent.js
+++ force-app/main/default/lwc/newsComponent/newsComponent.js
@@ -1,15 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import retrieveNews from "@salesforce/apex/NewsController.retrieveNews";
 
 export default class NewsComponent extends LightningElement {
+    @track result = []
     connectedCallback() {
         this.fetchNews();
     }
     fetchNews() {
         retrieveNews().then(response=>{
             console.log(response);
+            this.formatNewsData(response.articles);
         }).catch(error=>{
             console.error(error);
+        })
+    }
+    formatNewsData(res) {
+        this.result = res.map((item, index)=>{
+            let id = `new_${index+1}`;
+            let name = item.source.name;
+            return {...item, id:id, name:name}
         })
     }
 }
